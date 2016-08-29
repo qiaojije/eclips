1d90f9beef1a247ff30f6afbd06dcec304623c18
qiaojije <1044671330@qq.com>
2016-08-29 14:36:56 +0800

    0.0.3

--- src/com/baidu/Test.java
+++ src/com/baidu/Test.java
@@ -2,6 +2,7 @@ package com.baidu;
 
 public class Test {
      public static void main(String[] args) {
-		System.out.println("hello world!");
+		 System.out.println("√ª”–“≈∫∂");
+    	 System.out.println("hello world!");
 	}
 }
